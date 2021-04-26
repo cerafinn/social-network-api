2f1ee2229bbdf96235dd5ed33500858d84b27d93
cerafinn <74612014+cerafinn@users.noreply.github.com>
2021-04-26 15:47:21 -0400

    working on user routes

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -1,15 +1,20 @@
 const router = require('express').Router();
-const { getAllUsers, getUserById, createUser, updateUser, deleteUser } = require('../../controllers/user-controller')
+const { getAllUsers, getUserById, createUser, updateUser, deleteUser, addFriend, removeFriend } = require('../../controllers/user-controller')
 
 router
   .route('/')
   .get(getAllUsers)
-//   .post(createUser)
+  .post(createUser)
 
 router
   .route('/:id')
   .get(getUserById)
-//   .put(updateUser)
-//   .delete(deleteUser)
+  .put(updateUser)
+  .delete(deleteUser)
+
+router
+  .route('/:id/friends/:friendId')
+  .post(addFriend)
+  .delete(removeFriend)
 
 module.exports = router;
