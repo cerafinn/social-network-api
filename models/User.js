c385903cc9399045ebe947a5ddf2834863b462e3
cerafinn <74612014+cerafinn@users.noreply.github.com>
2021-04-26 12:50:26 -0400

    working on User Schema

--- models/User.js
+++ models/User.js
@@ -2,10 +2,26 @@ const { Schema, model } = require('mongoose');
 
 const UserSchema = new Schema(
   {
-    username: {},
-    email: {},
-    thoughts: {},
-    friends: {}
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      match: ['/^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/', 'Please enter a valid e-mail address']
+    },
+    thoughts: [{
+      type: Schema.Types.ObjectId,
+      ref: 'Thought'
+    }],
+    friends: [{
+      type: Schema.Types.ObjectId,
+      ref: 'User'
+    }]
   },
   {
     toJSON: {
