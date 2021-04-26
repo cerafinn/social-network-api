432e61b60dd0c61f28ca04b43f796359fb8a9a78
cerafinn <74612014+cerafinn@users.noreply.github.com>
2021-04-26 15:28:59 -0400

    working on user routes

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -2,6 +2,21 @@ const { User } = require('../models');
 
 const UserController = {
   // get all users
+  getAllUsers(req, res) {
+    User.find({})
+      .populate({
+        path: 'thoughts',
+        select: '-__v'
+      })
+      .select('-__v')
+      .sort({ _id: -1 })
+      .then(dbUserData => res.json(dbUserData))
+      .catch(err => {
+        console.log(err);
+        res.sendStatus(400);
+      });
+  },
+
   // get user by ID
   // add new user
   // add new friend
